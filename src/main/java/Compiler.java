d36e2fb5650b75165ef8147e03f320aa825f5aba
Beke András <beke.andras.mkolc@gmail.com>
2021-06-28 17:08:18 +0200

    Initial commit

---
+++ src/main/java/Compiler.java
@@ -0,0 +1,114 @@
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.beans.XMLDecoder;
+import java.io.File;
+
+public class Compiler {
+
+    String SourceFileName;
+    String PreprocessorFileName;
+    String XrefFileName;
+
+    public Compiler(String sourceFileName, String preprocessorFileName, String xrefFileName) {
+        SourceFileName = sourceFileName;
+        PreprocessorFileName = preprocessorFileName;
+        XrefFileName = xrefFileName;
+    }
+
+    public Compiler() {
+    }
+
+    public void CreateDemoFile() {
+        try {
+            DocumentBuilderFactory dbFactory =
+                    DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.newDocument();
+
+            // root element
+            Element rootElement = doc.createElement("networks");
+            doc.appendChild(rootElement);
+
+            // supercars element
+            Element network = doc.createElement("network");
+            rootElement.appendChild(network);
+
+            // setting attribute to element
+            Attr titleattr = doc.createAttribute("title");
+            titleattr.setValue("first network");
+            network.setAttributeNode(titleattr);
+
+            // setting attribute to element
+            Attr commentattr = doc.createAttribute("comment");
+            commentattr.setValue("This is the first network.");
+            network.setAttributeNode(commentattr);
+
+            // network element
+            Element boolElement = doc.createElement("AND");
+            Attr attrType = doc.createAttribute("ADDR");
+            attrType.setValue("0x5673");
+            boolElement.setAttributeNode(attrType);
+            network.appendChild(boolElement);
+
+            boolElement = doc.createElement("AND");
+            attrType = doc.createAttribute("ADDR");
+            attrType.setValue("0x22311");
+            boolElement.setAttributeNode(attrType);
+            network.appendChild(boolElement);
+
+            boolElement = doc.createElement("AND");
+            attrType = doc.createAttribute("CONST");
+            attrType.setValue("0x01");
+            boolElement.setAttributeNode(attrType);
+            network.appendChild(boolElement);
+
+
+
+
+            // write the content into xml file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(SourceFileName));
+            transformer.transform(source, result);
+
+            // Output to console for testing
+            StreamResult consoleResult = new StreamResult(System.out);
+            transformer.transform(source, consoleResult);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getSourceFileName() {
+        return SourceFileName;
+    }
+
+    public void setSourceFileName(String sourceFileName) {
+        SourceFileName = sourceFileName;
+    }
+
+    public String getPreprocessorFileName() {
+        return PreprocessorFileName;
+    }
+
+    public void setPreprocessorFileName(String preprocessorFileName) {
+        PreprocessorFileName = preprocessorFileName;
+    }
+
+    public String getXrefFileName() {
+        return XrefFileName;
+    }
+
+    public void setXrefFileName(String xrefFileName) {
+        XrefFileName = xrefFileName;
+    }
+}
---
+++ src/main/java/MainClass.java
@@ -0,0 +1,8 @@
+public class MainClass {
+
+    public static void main(String[] args) {
+        Compiler compiler = new Compiler();
+        compiler.setSourceFileName("C:\\Users\\beke_andras\\IdeaProjects\\PLCCompiler\\files\\testplcfile.plcsource");
+        compiler.CreateDemoFile();
+    }
+}
